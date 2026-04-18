24f7019dbdbb521638dab922230c466423ee1db3
Kacper7011 <kacper.elblag@vp.pl>
2026-04-18 19:09:01 +0200

    Migrate Terraform state to S3 backend (RustFS)

--- terraform/cloudflare/providers.tf
+++ terraform/cloudflare/providers.tf
@@ -5,6 +5,16 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "s3" {
+    bucket                      = "terraform"
+    key                         = "cloudflare/terraform.tfstate"
+    region                      = "us-east-1"
+    endpoint                    = "http://10.10.10.115:9020"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    force_path_style            = true
+  }
 }
 
 provider "cloudflare" {
--- terraform/vault/providers.tf
+++ terraform/vault/providers.tf
@@ -1,10 +1,20 @@
 terraform {
-    required_providers {
-      vault = {
-        source = "hashicorp/vault"
-        version = "~> 4.0"
-      }
+  required_providers {
+    vault = {
+      source = "hashicorp/vault"
+      version = "~> 4.0"
     }
+  }
+  backend "s3" {
+    bucket                      = "terraform"
+    key                         = "vault/terraform.tfstate"
+    region                      = "us-east-1"
+    endpoint                    = "http://10.10.10.115:9020"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    force_path_style            = true
+  }
 }
 
 provider "vault" {
