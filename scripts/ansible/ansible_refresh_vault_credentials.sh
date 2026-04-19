66815acf42029e31d99367f69e6c39e28c99b72d
Kacper7011 <kacper.elblag@vp.pl>
2026-04-20 00:13:38 +0200

    Fix Ansible secret_id refresh script and extend TTL to 10 days

--- scripts/ansible/ansible_refresh_vault_credentials.sh
+++ scripts/ansible/ansible_refresh_vault_credentials.sh
@@ -13,20 +13,16 @@ set -e
 
 ROOT_TOKEN=$(cat ~/.vault/root_token)
 LOG_DATE=$(date +"%Y-%m-%d")
-
-cd ~/homelab-platform/terraform/vault || { echo "Can't Enter ~/homelab-platform/terraform/vault"; exit 1; }
+LOG_FILE=~/.vault/ansible/ansible_refresh.log
 
 export VAULT_ADDR="http://10.10.10.216:8200"
 export VAULT_TOKEN="$ROOT_TOKEN"
 
-terraform destroy -target=vault_approle_auth_backend_role_secret_id.ansible -auto-approve
-terraform apply -auto-approve
-
-terraform output -raw ansible_secret_id > ~/.vault/ansible/secret_id
+vault write -f auth/approle/role/ansible/secret-id -format=json \
+    | jq -r '.data.secret_id' > ~/.vault/ansible/secret_id
 chmod 600 ~/.vault/ansible/secret_id
 
-touch ~/.vault/ansible/ansible_refresh.log
-
-echo "[ $LOG_DATE ] Ansible's secret_id has been refreshed" >> ~/.vault/ansible/ansible_refresh.log
+touch "$LOG_FILE"
+echo "[ $LOG_DATE ] Ansible's secret_id has been refreshed" >> "$LOG_FILE"
 
 
--- terraform/vault/approles.tf
+++ terraform/vault/approles.tf
@@ -10,7 +10,7 @@ resource "vault_approle_auth_backend_role" "ansible" {
     token_policies = [vault_policy.ansible.name]
     token_ttl = 3600
     token_max_ttl = 7200
-    secret_id_ttl = 604800
+    secret_id_ttl = 864000
     secret_id_num_uses = 0
 }
 
@@ -30,15 +30,15 @@ resource "vault_approle_auth_backend_role_secret_id" "ansible" {
 
 # Create AppRole role for kopia with read-only access to secrets
 
-resource "vault_approle_auth_backend_role" "kopia" {
+resource "vault_approle_auth_backend_role" "restic" {
     backend = vault_auth_backend.approle.path
-    role_name = "kopia"
-    token_policies = [vault_policy.kopia.name]
+    role_name = "restic"
+    token_policies = [vault_policy.restic.name]
     token_ttl = 3600
     token_max_ttl = 7200
 }
 
-resource "vault_approle_auth_backend_role_secret_id" "kopia" {
+resource "vault_approle_auth_backend_role_secret_id" "restic" {
     backend = vault_auth_backend.approle.path
-    role_name = vault_approle_auth_backend_role.kopia.role_name
+    role_name = vault_approle_auth_backend_role.restic.role_name
 }
