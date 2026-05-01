37641e85c4c01c4145c86c51183c6cf6afe2ea30
kacper7011 <kacper.elblag@vp.pl>
2026-05-01 18:18:49 +0200

    Migrate seafile from docker to kubernetes

--- terraform/vault/kubernetes.tf
+++ terraform/vault/kubernetes.tf
@@ -5,6 +5,10 @@ resource "vault_kubernetes_auth_backend_config" "kubernetes" {
     token_reviewer_jwt = var.vault_k8s_rewiever_token
 }
 
+# Vault policies and roles for kubernetes authentication
+
+# Vault policy and role for cloudflared
+
 resource "vault_policy" "cloudflared" {
     name = "cloudflared"
     policy = <<-EOT
@@ -20,5 +24,25 @@ resource "vault_kubernetes_auth_backend_role" "cloudflared" {
     bound_service_account_names = ["cloudflared-sa"]
     bound_service_account_namespaces = ["cloudflared"]
     token_policies = [vault_policy.cloudflared.name]
+    token_ttl = 3600
+}
+
+# Vault policy and role for seafile
+
+resource "vault_policy" "seafile" {
+    name = "seafile"
+    policy = <<-EOT
+        path "secret/data/services/seafile" {
+            capabilities = ["read"]
+        }
+    EOT
+}
+
+resource "vault_kubernetes_auth_backend_role" "seafile" {
+    backend = vault_auth_backend.kubernetes.path
+    role_name = "seafile"
+    bound_service_account_names = ["seafile-sa"]
+    bound_service_account_namespaces = ["seafile"]
+    token_policies = [vault_policy.seafile.name]
     token_ttl = 3600
 }
